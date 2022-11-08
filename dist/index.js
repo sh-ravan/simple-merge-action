1f20aed3f803899cf697c99036eff8833f09de18
Shravan <83463078+sh-ravan@users.noreply.github.com>
2022-11-09 00:04:33 +0530

    init

--- dist/index.js
+++ dist/index.js
@@ -2688,19 +2688,10 @@ exports.default = _default;
 
 /***/ }),
 
-/***/ 258:
+/***/ 716:
 /***/ ((module) => {
 
-let wait = function (milliseconds) {
-  return new Promise((resolve) => {
-    if (typeof milliseconds !== 'number') {
-      throw new Error('milliseconds not a number');
-    }
-    setTimeout(() => resolve("done!"), milliseconds)
-  });
-};
-
-module.exports = wait;
+module.exports = eval("require")("@actions/github");
 
 
 /***/ }),
@@ -2835,20 +2826,68 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const core = __nccwpck_require__(186);
-const wait = __nccwpck_require__(258);
-
+const github = __nccwpck_require__(716);
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+    const owner =
+      core.getInput("owner") ||
+      (process.env.GITHUB_REPOSITORY || "").split("/")[0];
+    const repo =
+      core.getInput("repo") ||
+      (process.env.GITHUB_REPOSITORY || "").split("/")[1];
+    const head = core.getInput("source-branch");
+    const base = core.getInput("target-branch");
+    const commit_message =
+      core.getInput("commit-message") || `Automatic Merge - ${head} -> ${base}`;
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    core.debug(new Date().toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const octokit = github.getOctokit(GITHUB_TOKEN);
 
-    core.setOutput('time', new Date().toTimeString());
+    const res = await octokit.repos.merge({
+      owner,
+      repo,
+      base,
+      head,
+      commit_message,
+    });
+
+    if (res && res.data) {
+      switch (res.status) {
+        case 201:
+          core.info(`Merged ${head} -> ${base} (${res.data.sha || ""})`);
+          break;
+
+        case 204:
+          core.info(
+            "Target branch already contains changes from source branch. Nothing to merge"
+          );
+          break;
+
+        case 409:
+          core.setFailed(`Merge conflict. ${res.data.message || ""}`);
+          break;
+
+        case 404:
+          core.setFailed(`Branch not found. ${res.data.message || ""}`);
+          break;
+
+        default:
+          core.warning(
+            `Merge action has completed, but with an unknown status code: ${res.status}`
+          );
+      }
+    } else {
+      return core.setFailed(
+        "An unknown error occurred during merge operation (empty response)"
+      );
+    }
+
+    core.info(new Date().toTimeString());
+
+    core.setOutput("time", new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
--- index.js
+++ index.js
@@ -1,18 +1,62 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+    const owner =
+      core.getInput("owner") ||
+      (process.env.GITHUB_REPOSITORY || "").split("/")[0];
+    const repo =
+      core.getInput("repo") ||
+      (process.env.GITHUB_REPOSITORY || "").split("/")[1];
+    const head = core.getInput("source-branch");
+    const base = core.getInput("target-branch");
+    const commit_message =
+      core.getInput("commit-message") || `Automatic Merge - ${head} -> ${base}`;
+    const octokit = github.getOctokit(GITHUB_TOKEN);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const res = await octokit.repos.merge({
+      owner,
+      repo,
+      base,
+      head,
+      commit_message,
+    });
 
-    core.setOutput('time', new Date().toTimeString());
+    if (res && res.data) {
+      switch (res.status) {
+        case 201:
+          core.info(`Merged ${head} -> ${base} (${res.data.sha || ""})`);
+          break;
+
+        case 204:
+          core.info(
+            "Target branch already contains changes from source branch. Nothing to merge"
+          );
+          break;
+
+        case 409:
+          core.setFailed(`Merge conflict. ${res.data.message || ""}`);
+          break;
+
+        case 404:
+          core.setFailed(`Branch not found. ${res.data.message || ""}`);
+          break;
+
+        default:
+          core.warning(
+            `Merge action has completed, but with an unknown status code: ${res.status}`
+          );
+      }
+    } else {
+      return core.setFailed(
+        "An unknown error occurred during merge operation (empty response)"
+      );
+    }
+
+    core.setOutput("time", new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
